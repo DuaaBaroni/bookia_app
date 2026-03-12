4b648bb05bae5357cf2d3a2a7efd672ad03009f0
DuaaBaroni <duaabaroni136@gmail.com>
2026-03-12 23:00:48 +0200

    modified_bestSeller

--- lib/features/home/presentation/widgets/best_seller.dart
+++ lib/features/home/presentation/widgets/best_seller.dart
@@ -37,19 +37,7 @@ class BestSellerBooks extends StatelessWidget {
             ],
           );
         } else {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // TextShimmer(width: 100),
-              // Gap(20),
-              // GridShimmer(
-              //   crossAxisCount: 2,
-              //   mainAxisSpacing: 11,
-              //   crossAxisSpacing: 11,
-              //   childAspectRatio: .6,
-              // ),
-            ],
-          );
+          return Column();
         }
       },
     );
